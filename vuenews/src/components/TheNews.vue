77f2f70bdbafdc813cc2c9f7c7fbc9fc83f61833
Mikhail Zavoloka <mikhail.zavoloka@gmail.com>
2024-06-10 19:22:49 +0300

    Mark as read button and desing

--- vuenews/src/components/TheNews.vue
+++ vuenews/src/components/TheNews.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { ref, onMounted } from 'vue'
+  import { ref, onMounted, toRaw } from 'vue'
 
   const slitems = ref([])
   const ytitems = ref([])
@@ -18,39 +18,54 @@
   }
 
   function refreshItems(allitems) {
-    tgitems.value = allitems.tg_news;
-    ytitems.value = allitems.yt_news;
-    slitems.value = allitems.sl_news;
-    otitems.value = allitems.ot_news;
+    tgitems.value = allitems.tg_news.map( x => ({ ...x, marked_as_read: localStorage.getItem(x.id) }) )
+    ytitems.value = allitems.yt_news.map( x => ({ ...x, marked_as_read: localStorage.getItem(x.id) }) )
+    slitems.value = allitems.sl_news.map( x => ({ ...x, marked_as_read: localStorage.getItem(x.id) }) )
+    otitems.value = allitems.ot_news.map( x => ({ ...x, marked_as_read: localStorage.getItem(x.id) }) )
+  }
+
+  function mark_all_as_read() {
+    const mark = (el) => {
+      localStorage.setItem(el.id, true)
+      if ( !el.marked_as_read ) { el.marked_as_read = true }
+    }
+    tgitems.value.forEach(el => mark(el))
+    ytitems.value.forEach(el => mark(el))
+    slitems.value.forEach(el => mark(el))
+    otitems.value.forEach(el => mark(el))
   }
 </script>
 
 <template>
   <div class="wrapper">
     <div class="tg">
-      <div v-for="tg in tgitems" :key="tg.id" class="tgitem" :class="tg.recency" v-html="tg.content"></div>
+      <div v-for="tg in tgitems" :key="tg.id" class="tgitem" :class="{ marked_as_read: tg.marked_as_read }" v-html="tg.content"></div>
       <!--<p style="display: none;">{{tg.hdate}} <a :href="tg.url">{{tg.url}}></a></p>-->
     </div>
     <div class="yt">
-      <div v-for="yt in ytitems" :key="yt.id" class="ytitem" :class="yt.recency">
-        <small><a :href="yt.url">{{yt.title}}</a>{{yt.hdate}} {{yt.author}}</small>
+      <button id="mark_all_as_read" @click="mark_all_as_read">Mark all as read</button>
+      <div v-for="yt in ytitems" :key="yt.id" class="ytitem" :class="{ marked_as_read: yt.marked_as_read }">
+        <small>{{yt.author}}</small><br/>
+        <small>{{yt.hdate}}</small><br/>
+        <a :href="yt.url">{{yt.title}}</a>
         <p style="display: none;">{{yt.content}}</p>
       </div>
     </div>
 
     <div class="sl">
-      <div v-for="sl in slitems" :key="sl.id" class="slitem" :class="sl.recency">
-        <a :href="sl.url">{{sl.title}}</a>
-        <small><b>{{sl.author}}</b>&nbsp;{{sl.hdate}}</small></br>
-        <small v-html="sl.content"></small>
+      <div v-for="sl in slitems" :key="sl.id" class="slitem" :class="{ marked_as_read: sl.marked_as_read }">
+        <small><b>{{sl.author}}</b> &nbsp; {{sl.hdate}}</small><br/>
+        <a :href="sl.url">{{sl.title}}</a><br/>
+        <span v-html="sl.content"></span>
       </div>
     </div>
 
     <div class="ot">
       <h4 style="display: none;">Others</h4>
-      <div v-for="ot in otitems" :key="ot.id" class="otitem" :class="ot.recency">
-        <a :href="ot.url">{{ot.title}}</a>{{ot.hdate}}</br>
-        <small v-html="ot.content"></small>
+      <div v-for="ot in otitems" :key="ot.id" class="otitem" :class="{ marked_as_read: ot.marked_as_read }">
+        <small>{{ot.url.match(/:\/\/(.[^\/]+)(.*)/)[1]}} &nbsp; {{ot.hdate}}</small><br/>
+        <a :href="ot.url">{{ot.title}}</a><br/>
+        <span v-html="ot.content"></span>
       </div>
     </div>
   </div>
